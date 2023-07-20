00f62a65be2b1e56b2e469b0cf2ec542fa17ae9c
Umenorin <umenorin@gmail.com>
2023-07-19 22:37:02 -0300

    Implementing the Position Class

--- Tic-Tac-Toe/GameBoard/Position.cs
+++ Tic-Tac-Toe/GameBoard/Position.cs
@@ -1,5 +1,15 @@
 ﻿namespace GameBoard {
     internal class Position {
+        public int Row { get; private set; }
+        public int Column { get; private set; }
 
+        public Position(int row, int column) {
+            Row = row;
+            Column = column;
+        }
+        public void SetPosition(int row, int column) {
+            this.Row = row;
+            this.Column = column;
+        }
     }
 }
