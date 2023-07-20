0cd2b77b50da3b1592752695d09fe37b5d18e4df
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-20 16:02:33 -0300

    Starting to implement MatchPosition class

--- Tic-Tac-Toe/Match/MatchPosition.cs
+++ Tic-Tac-Toe/Match/MatchPosition.cs
@@ -1,5 +1,27 @@
-﻿namespace match {
-    internal class MatchPosition {
+﻿using gameBoard;
+using System.Text;
 
+namespace match {
+    internal class MatchPosition {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public MatchPosition(int row, char column) {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition() {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString() {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Column);
+            sb.Append(Row);
+
+            return sb.ToString();
+        }
     }
 }
