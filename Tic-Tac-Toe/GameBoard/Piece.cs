6219c74fdfc0f718946c75c2442768ed3d4a38f4
Gustavo Monnerat <gugammcv@gmail.com>
2023-07-19 22:32:23 -0300

    Implementation of the piece class

--- Tic-Tac-Toe/GameBoard/Piece.cs
+++ Tic-Tac-Toe/GameBoard/Piece.cs
@@ -1,5 +1,13 @@
-﻿namespace GameBoard {
-    internal class Piece {
+﻿using GameBoard.Enums;
 
+namespace GameBoard {
+    internal class Piece {
+        public Symbol Symbol { get; private set; }
+        public Position Position { get; private set; }
+
+        public Piece(Symbol symbol, Position position) {
+            Symbol = symbol;
+            Position = position;
+        }
     }
 }
